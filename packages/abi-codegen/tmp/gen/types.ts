d0c53e76d9ed6187fcdd531eb47b1f6045ae2857
xilosada <xabi@calimero.network>
2025-08-13 15:43:30 +0200

    style: apply prettier formatting to generated files

--- packages/abi-codegen/tmp/gen/types.ts
+++ packages/abi-codegen/tmp/gen/types.ts
@@ -8,21 +8,30 @@ export interface AbiState {
 export type ActionPayload =
   | { name: 'Ping' }
   | { name: 'SetName'; payload: string }
-  | { name: 'Update'; payload: UpdatePayload }
+  | { name: 'Update'; payload: UpdatePayload };
 
 export const Action = {
   Ping: (): ActionPayload => ({ name: 'Ping' }),
-  SetName: (setname: string): ActionPayload => ({ name: 'SetName', payload: setname }),
-  Update: (update: UpdatePayload): ActionPayload => ({ name: 'Update', payload: update }),
+  SetName: (setname: string): ActionPayload => ({
+    name: 'SetName',
+    payload: setname,
+  }),
+  Update: (update: UpdatePayload): ActionPayload => ({
+    name: 'Update',
+    payload: update,
+  }),
 } as const;
 
 export type ConformanceErrorPayload =
   | { name: 'BadInput' }
-  | { name: 'NotFound'; payload: string }
+  | { name: 'NotFound'; payload: string };
 
 export const ConformanceError = {
   BadInput: (): ConformanceErrorPayload => ({ name: 'BadInput' }),
-  NotFound: (notfound: string): ConformanceErrorPayload => ({ name: 'NotFound', payload: notfound }),
+  NotFound: (notfound: string): ConformanceErrorPayload => ({
+    name: 'NotFound',
+    payload: notfound,
+  }),
 } as const;
 
 /** Fixed-length bytes (size: 64). Represented as string at runtime. */
@@ -48,18 +57,17 @@ export interface UpdatePayload {
   data: string;
 }
 
-export type may_failErrorCode = "BAD_INPUT" | "NOT_FOUND";
+export type may_failErrorCode = 'BAD_INPUT' | 'NOT_FOUND';
 export type may_failError = { code: may_failErrorCode } & (
-  | { code: "BAD_INPUT" }
-  | { code: "NOT_FOUND"; payload: string }
+  | { code: 'BAD_INPUT' }
+  | { code: 'NOT_FOUND'; payload: string }
 );
 
-export type find_personErrorCode = "BAD_INPUT" | "NOT_FOUND";
+export type find_personErrorCode = 'BAD_INPUT' | 'NOT_FOUND';
 export type find_personError = { code: find_personErrorCode } & (
-  | { code: "BAD_INPUT" }
-  | { code: "NOT_FOUND"; payload: string }
+  | { code: 'BAD_INPUT' }
+  | { code: 'NOT_FOUND'; payload: string }
 );
-
 
 export type NamedPayload = string;
 
@@ -70,9 +78,8 @@ export type PersonUpdatedPayload = string;
 export type ActionTakenPayload = string;
 
 export type AbiEvent =
-  | { name: "Ping" }
-  | { name: "Named"; payload: string }
-  | { name: "Data"; payload: Uint8Array }
-  | { name: "PersonUpdated"; payload: string }
-  | { name: "ActionTaken"; payload: string }
-;
+  | { name: 'Ping' }
+  | { name: 'Named'; payload: string }
+  | { name: 'Data'; payload: Uint8Array }
+  | { name: 'PersonUpdated'; payload: string }
+  | { name: 'ActionTaken'; payload: string };
